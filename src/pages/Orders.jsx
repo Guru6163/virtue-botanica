a936716fd4fe044e3f79913a06cc81fa8e4c7f46
GuruF <guruprasath@aidaptive.com>
2024-04-25 17:55:11 +0530

    uu

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -117,9 +117,9 @@ function Orders() {
             <ConfirmPopup />
             <DataTable showGridlines value={orders} loading={loading} emptyMessage="No Orders Available">
                 <Column alignHeader='center' align='center' field="orderId" header="Order ID" sortable />
-                <Column header="Customer Name" body={rowData => `${rowData.customerDetails?.firstName ?? 'N/A'} ${rowData.customerDetails?.lastName ?? 'N/A'}`} sortable />
+                <Column alignHeader='center' align='center' header="Customer Name" body={rowData => `${rowData.customerDetails?.firstName ?? 'N/A'} ${rowData.customerDetails?.lastName ?? 'N/A'}`} sortable />
                 <Column alignHeader='center' align='center' field="timestamp" header="Ordered At" body={rowData => rowData.timestamp ? new Date(rowData.timestamp.toDate()).toLocaleString('en-US', { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true }) : 'N/A'} sortable />
-                <Column alignHeader='center' field="customerDetails.phone" header="Phone Number" sortable />
+                <Column alignHeader='center' align='center' field="customerDetails.phone" header="Phone Number" sortable />
                 <Column alignHeader='center' align='center' header="Items Count" body={rowData => rowData.items ? rowData.items.reduce((total, item) => total + parseInt(item.quantity), 0) : 'N/A'} sortable />
                 <Column alignHeader='center' align='center' field="totalPrice" header="Cost" body={rowData => `Rs.${rowData.totalPrice ?? 'N/A'}`} sortable />
                 <Column alignHeader='center' align='center' className='capitalize' field="deliveryStatus" header="Delivery Status" body={rowData => rowData.deliveryStatus ?? 'N/A'} sortable />
